747662b262bb6d21f60ac08b44637910c35045a3
João Ribeiro <97030703+joao-ribeirooo@users.noreply.github.com>
2023-06-29 18:15:46 +0100

    Create bioproject_download_and_compress.sh

---
+++ bioproject_download_and_compress.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd
+cd Documents
+mkdir -p Bioproject
+cd Bioproject
+
+#filename="$HOME/Documents/TESTE.csv"
+
+while read line
+do
+    fasterq-dump ${line}
+    bgzip ${line}_1.fastq
+    bgzip ${line}_2.fastq
+done < "/home/ribeiroriri/Documents/TESTE.csv"
