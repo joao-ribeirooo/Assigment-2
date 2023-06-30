cc3366b8fc5888a7abb3882a6005dea75454d67d
João Ribeiro <97030703+joao-ribeirooo@users.noreply.github.com>
2023-06-30 17:46:39 +0100

    Update bioproject_file_processing.sh

--- bioproject_file_processing.sh
+++ bioproject_file_processing.sh
@@ -3,14 +3,12 @@
 cd 
 cd Documents
 cd Bioproject
+mkdir cut
 
 mypath=`pwd`
-
 
 for filename in $(ls $mypath | grep -v cut | grep -v ipyrad-assembly)
 do
     echo ${filename}
     zcat ${filename} | head -n 2000000 | bgzip > cut/${filename}
-    #echo $filename
-    #echo cut/$filename
 done
